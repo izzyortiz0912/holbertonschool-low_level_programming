b8cc5c9c53826f00480ce4a23b802014e6293727
izzyortiz0912 <izzyortiz0912@gmail.com>
2025-04-05 16:43:00 -0500

    changes made

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-int main(void)
-{
-
- //string intialisation
- char Mystr[] = "Programming is like building a multilingual puzzle";
-
- puts(Mystr); //writing string to stdout 
-
- return(0);
-
-}
+ #include <stdio.h>
+ int main(void)
+ {
+ 
+ /*string intialisation */
+ char Mystr[] = "\"Programming is like building a multilingual puzzle";
+ 
+ puts(Mystr); /* writing string to stdout */
+ 
+  return(0);
+ 
+ }
+  
