b2b32a2b7074c50ab776503d203ea404d231aec0
izzyortiz0912 <izzyortiz0912@gmail.com>
2025-04-05 17:46:12 -0500

    C program that prints exactly Programming is like building a multilingual puzzle

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,16 @@
- #include <stdio.h>
- int main(void)
- {
- 
- /*string intialisation */
- char Mystr[] = "\"Programming is like building a multilingual puzzle";
- 
- puts(Mystr); /* writing string to stdout */
- 
-  return(0);
- 
- }
-  
+#include <stdio.h>
+/**
+*   main - Entry point program that prints exactly
+*  "Programming is like building a multilingual puzzle, followed by a new line.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+/*string intialisation */
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+puts(Mystr); /* writing string to stdout */
+
+return (0);
+}
