62da908cd500d8d9bcc65c84c635a73999e94212
izzyortiz0912 <izzyortiz0912@gmail.com>
2025-04-09 15:39:19 -0500

    Writing source code in order to print whether the number stored in the variable n is positive or negative

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Prints whether a randomly generated number
+* is positive, negative, or zero.
+*
+* Return: 0 (Always success)
+*/
+
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is postive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+return (0);
+
+}
